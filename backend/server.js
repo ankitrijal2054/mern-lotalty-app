0c133ec3b0208615d8bc49ef7cad0f56fe41f1ab
ankitrijal2054 <ankitrijal2054@gmail.com>
2021-12-15 20:54:40 -0600

    model and routes created

--- backend/server.js
+++ backend/server.js
@@ -20,10 +20,15 @@ app.listen(port, () => {
       console.log(`Server running on port: ${port}`);
 });
 */
-
-//const CONNECTION_URL = 'mongodb+srv://mernproject:mernproject123@cluster0.4aqft.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+const CONNECTION_URL = 'mongodb+srv://mernproject:mernproject123@cluster0.4aqft.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true})
     .then(() => app.listen(PORT, () => console.log('Server running on port: ' + PORT)))
     .catch((error) => console.log(error.message));
+
+const exercisesRouter = require('./routes/exercises');
+const usersRouter = require('./routes/users');
+
+app.use('/exercises', exercisesRouter);
+app.use('/users', usersRouter);
